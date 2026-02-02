42bcff77081356499f27515222b210095a20adb4
anthony.degroote <anthony.degroote@viacesi.fr>
2026-02-02 20:34:43 +0100

    Dry bills

--- cash_cleaner.py
+++ cash_cleaner.py
@@ -1,22 +1,29 @@
 from dataclasses import dataclass
 
-def condition(bill: Bill): 
-    return bill.amount == 100 and bill.currency == "$"
-
 def validate_hundred(bill: Bill):
     return bill.amount == 100
 
 def validate_dollar(bill: Bill):
     return bill.currency == "$"
 
+def validate_legitemite(bill: Bill):
+    return bill.is_authentic
+
 def cash_cleaner_machine(bill_stack: list[Bill]) -> dict[list, list]:
-    validators = [validate_hundred, validate_dollar]
+    validators = [validate_hundred, validate_dollar, validate_legitemite]
     return {
         "accepted":[bill for bill in bill_stack if all(v(bill) for v in validators)],
         "rejected":[bill for bill in bill_stack if not all(v(bill) for v in validators)] 
     }
 
+def dry_machine (bill_stack: list[Bill]) -> list:
+    for bill in bill_stack:
+        bill.is_dry = True
+    return bill_stack
+
 @dataclass
 class Bill:
     amount: int
     currency: str = "$"
+    is_authentic: bool = True
+    is_dry: bool = True
--- test_cash_cleaner.py
+++ test_cash_cleaner.py
@@ -1,4 +1,4 @@
-from cash_cleaner import Bill, cash_cleaner_machine
+from cash_cleaner import Bill, cash_cleaner_machine, dry_machine
 
 
 def test_return_hundred_bills():
@@ -17,3 +17,17 @@ def test_return_hundred_dollar_bills():
         "accepted":[Bill(100, "$")], 
         "rejected":[Bill(100, "€"), Bill(20, "$"), Bill(50, "€")]
     } 
+
+def test_return_hundred_dollar_authentic_bills():
+
+    bill_stack = [Bill(100, "$", False), Bill(20, "$"), Bill(50, "€"), Bill(100, "$")]
+
+    assert cash_cleaner_machine(bill_stack) == {
+        "accepted":[Bill(100, "$")], 
+        "rejected":[Bill(100, "$", False), Bill(20, "$"), Bill(50, "€")]
+    } 
+
+def test_dry_bills():
+    bill_stack = [Bill(100), Bill(20, is_dry=False), Bill(50), Bill(100, is_dry=False)]
+
+    assert dry_machine(bill_stack) == [Bill(100), Bill(20), Bill(50), Bill(100)]
