c11b0fa9df7ebb96c02484105e71ad3ae4da0602
anthony.degroote <anthony.degroote@viacesi.fr>
2026-02-02 20:11:51 +0100

    Add Validators

--- cash_cleaner.py
+++ cash_cleaner.py
@@ -3,10 +3,17 @@ from dataclasses import dataclass
 def condition(bill: Bill): 
     return bill.amount == 100 and bill.currency == "$"
 
+def validate_hundred(bill: Bill):
+    return bill.amount == 100
+
+def validate_dollar(bill: Bill):
+    return bill.currency == "$"
+
 def cash_cleaner_machine(bill_stack: list[Bill]) -> dict[list, list]:
+    validators = [validate_hundred, validate_dollar]
     return {
-        "accepted":[bill for bill in bill_stack if condition(bill)],
-        "rejected":[bill for bill in bill_stack if not condition(bill)] 
+        "accepted":[bill for bill in bill_stack if all(v(bill) for v in validators)],
+        "rejected":[bill for bill in bill_stack if not all(v(bill) for v in validators)] 
     }
 
 @dataclass
